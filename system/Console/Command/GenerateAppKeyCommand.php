338f55624bbabe39285bbe7421862abdc3196752
SupianIDz <supianidz@gmail.com>
2019-04-10 13:06:12 +0800

    Commit Wed Apr 10 13:06:12 2019

---
+++ system/Console/Command/GenerateAppKeyCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ *   ___       _
+ *  / _ \  ___| |_ ___  _ __  _   _
+ * | | | |/ __| __/ _ \| '_ \| | | |
+ * | |_| | (__| || (_) | |_) | |_| |
+ *  \___/ \___|\__\___/| .__/ \__, |
+ *                     |_|    |___/
+ * @author  : Supian M <supianidz@gmail.com>
+ * @link    : www.octopy.xyz
+ * @license : MIT
+ */
+
+namespace Octopy\Console\Command;
+
+use Exception;
+
+use Octopy\Console\Argv;
+use Octopy\Console\Output;
+use Octopy\Console\Command;
+use Octopy\Encryption\Encrypter;
+
+class GenerateAppKeyCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'key:generate';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Set the application key';
+    
+    /**
+     * @param  Argv   $argv
+     * @param  Output $output
+     * @return string
+     */
+    public function handle(Argv $argv, Output $output)
+    {
+        $key = $this->key();
+
+        if ($argv->get('--show')) {
+            return $output->warning($key);
+        }
+
+        // Next, we will replace the application key in the environment file so it is
+        // automatically setup for this developer. This key gets generated using a
+        // secure random byte generator and is later base64 encoded for storage.
+        $this->write($key);
+      
+        $this->app['config']['app.key'] = $key;
+
+        return $output->success('Application key set successfully.');
+    }
+
+    /**
+     * @return string
+     */
+    protected function key() : string
+    {
+        return 'base64:'.base64_encode(
+            Encrypter::generate($this->app['config']['app.cipher'])
+        );
+    }
+
+    /**
+     * @param  string  $key
+     * @return void
+     */
+    protected function write($key)
+    {
+        $lines = file($environment = $this->app->basepath() . '.env');
+        foreach ($lines as $i => $line) {
+            if (preg_match('/^APP_KEY/', $line = trim($line))) {
+                $array    = explode('=', $line, 2);
+                $array[1] = $key;
+                $line     = implode('= ', $array);
+            }
+
+            $lines[$i] = $line;
+        }
+
+        file_put_contents($environment, implode("\n", $lines));
+    }
+}
---
+++ system/Provider/EncryptionServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ *   ___       _
+ *  / _ \  ___| |_ ___  _ __  _   _
+ * | | | |/ __| __/ _ \| '_ \| | | |
+ * | |_| | (__| || (_) | |_) | |_| |
+ *  \___/ \___|\__\___/| .__/ \__, |
+ *                     |_|    |___/
+ * @author  : Supian M <supianidz@gmail.com>
+ * @link    : www.octopy.xyz
+ * @license : MIT
+ */
+
+namespace Octopy\Provider;
+
+use RuntimeException;
+
+use Octopy\Encryption\Encrypter;
+
+class EncryptionServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->console()) {
+            return;
+        }
+        
+        $key = $this->key(
+            $config = $this->app['config']['app']
+        );
+
+        $this->app->instance('encrypter', new Encrypter($key, $config['cipher']));
+    }
+
+    /**
+     * @param  array $config
+     * @return string
+     */
+    protected function key(array $config) : string
+    {
+        if (empty($key = $config['key'])) {
+            throw new RuntimeException(
+                'No application encryption key has been specified.'
+            );
+        }
+
+        if (preg_match('/base64:/', $key)) {
+            $key = base64_decode(substr($key, 7));
+        }
+
+        return $key;
+    }
+}
